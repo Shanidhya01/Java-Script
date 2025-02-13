7a760dbf30a4a84cdc69393bcb7d0cc239d39e85
Shanidhya kumar <152613465+Shanidhya01@users.noreply.github.com>
2025-02-13 17:28:59 +0000

    datatype

--- BASICS/datatype.js
+++ BASICS/datatype.js
@@ -1,2 +1,27 @@
 "use strict"  // Treat all JS code as newer version
 
+// alert(3+3)  // we are using node js not browser
+
+console.log(3+3);
+console.log("Shanidhya");
+
+let name = "Shanidhya"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2^53
+// bigint used when large number
+// string => ""
+// boolean => true false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
